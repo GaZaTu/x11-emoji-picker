6d98734edaf85bb6d3286dbcaee771525d2c31b3
GaZaTu <gazatu@protonmail.com>
2021-05-11 19:17:09 +0200

    remove package.sh

--- package.sh
+++
@@ -1,7 +0,0 @@
-mkdir -p build
-cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j6
-make install DESTDIR=AppDir
-~/bin/linuxdeployqt.AppImage AppDir/usr/share/applications/emoji-picker.desktop -appimage -no-translations
-cd ..
