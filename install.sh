17ccfc66e6dd342ba1aa183dec1bf59f76c4a4a9
GaZaTu <gazatu@protonmail.com>
2021-11-23 23:14:09 +0100

    fix opensuse & appimage in install.sh

--- install.sh
+++ install.sh
@@ -1,44 +1,57 @@
 #!/bin/sh
 
-RELEASE=latest
+PROJECT_OWNER=GaZaTu
+PROJECT_REPO=x11-emoji-picker
+PROJECT_NAME=$PROJECT_REPO
+
+RELEASE="latest"
 
 while getopts ":r:" flag
 do
   case $flag in
-    r) RELEASE=${OPTARG};;
+    r) RELEASE="$OPTARG";;
   esac
 done
 
-echo "Installing x11-emoji-picker release: $RELEASE..."
+echo "Installing $PROJECT_NAME release: $RELEASE..."
 
 DISTRO_ID=$(grep "^ID=" "/etc/os-release" | sed "s/ID=//" | sed "s/\"//g")
 DISTRO_VERSION_ID=$(grep "^VERSION_ID=" "/etc/os-release" | sed "s/VERSION_ID=//" | sed "s/\"//g")
 
 PACKAGE_MANAGER=$(
-  case $DISTRO_ID in
-    ubuntu | debian) echo apt;;
-    opensuse) echo zypper;;
-    fedora) echo dnf;;
+  case "$DISTRO_ID" in
+    "ubuntu"* | "debian"*) echo "apt";;
+    "opensuse"*) echo "zypper";;
+    "fedora"*) echo "dnf";;
+  esac
+)
+PACKAGE_MANAGER_INSTALL_COMMAND=$(
+  case "$PACKAGE_MANAGER" in
+    "apt") echo "install -y";;
+    "zypper") echo "install -y --allow-unsigned-rpm";;
+    "dnf") echo "install -y --nogpgcheck";;
   esac
 )
 
-if [ -z "$PACKAGE_MANAGER" ];
-then
-  echo "No known package manager found"
-  exit 1
-fi
-
 if ! command -v jq > /dev/null;
 then
+  if [ -z "$PACKAGE_MANAGER" ];
+  then
+    echo "jq not found, and no known package manager found to install it either"
+    exit 1
+  fi
+
   echo ""
   echo "jq not found, installing through package manager..."
-  sudo $PACKAGE_MANAGER install -y jq
+  echo "> sudo $PACKAGE_MANAGER $PACKAGE_MANAGER_INSTALL_COMMAND jq"
+  sudo $PACKAGE_MANAGER $PACKAGE_MANAGER_INSTALL_COMMAND jq
 fi
 
 echo ""
-echo "Ask github for the latest version of x11-emoji-picker..."
-RELEASE_JSON="/tmp/x11-emoji-picker-$RELEASE.json"
-wget -q "https://api.github.com/repos/GaZaTu/x11-emoji-picker/releases/$RELEASE" -O "$RELEASE_JSON"
+echo "Check github for release artifacts..."
+RELEASE_ID=$(echo $RELEASE | sed "s/\//-/")
+RELEASE_JSON="/tmp/$PROJECT_REPO-$RELEASE_ID.json"
+wget -nv "https://api.github.com/repos/$PROJECT_OWNER/$PROJECT_REPO/releases/$RELEASE" -O "$RELEASE_JSON"
 
 echo ""
 echo "Find artifact for $DISTRO_ID $DISTRO_VERSION_ID"
@@ -62,7 +75,9 @@ ARTIFACT_NAME=$(basename "$URL")
 ARTIFACT_PATH="/tmp/$ARTIFACT_NAME"
 
 EXECUTABLE_NAME="emoji-picker"
-EXECUTABLE_NAME_APPIMAGE="emoji-picker.AppImage"
+
+APPIMAGE_NAME="$EXECUTABLE_NAME.AppImage"
+APPIMAGE_INSTALL_PATH="/usr/bin/$APPIMAGE_NAME"
 
 echo ""
 echo "Downloading artifact..."
@@ -70,25 +85,34 @@ wget -q --show-progress "$URL" -O "$ARTIFACT_PATH"
 
 if $ARTIFACT_IS_APPIMAGE
 then
-  EXECUTABLE_NAME="$EXECUTABLE_NAME_APPIMAGE"
-  APPIMAGE_INSTALL_PATH="/usr/bin/$EXECUTABLE_NAME"
+  if command -v $EXECUTABLE_NAME > /dev/null;
+  then
+    echo ""
+    echo "Abort: do not override $EXECUTABLE_NAME with .AppImage"
+    exit 2
+  fi
+
+  EXECUTABLE_NAME="$APPIMAGE_NAME"
 
   echo ""
   echo "Move artifact and make it executable"
-  echo "sudo cp $ARTIFACT_PATH $APPIMAGE_INSTALL_PATH"
+  echo "> sudo cp $ARTIFACT_PATH $APPIMAGE_INSTALL_PATH"
   sudo cp "$ARTIFACT_PATH" "$APPIMAGE_INSTALL_PATH"
-  echo "sudo chmod +x $APPIMAGE_INSTALL_PATH"
+  echo "> sudo chmod +x $APPIMAGE_INSTALL_PATH"
   sudo chmod +x "$APPIMAGE_INSTALL_PATH"
 else
+  if command -v $APPIMAGE_NAME > /dev/null;
+  then
+    echo ""
+    echo "Remove existing .AppImage"
+    echo "> sudo rm $APPIMAGE_INSTALL_PATH"
+    sudo rm "$APPIMAGE_INSTALL_PATH"
+  fi
+
   echo ""
   echo "Install artifact using package manager"
-  echo "sudo $PACKAGE_MANAGER install -y $ARTIFACT_PATH"
-  sudo $PACKAGE_MANAGER install -y "$ARTIFACT_PATH"
-
-  if command -v $EXECUTABLE_NAME_APPIMAGE > /dev/null;
-  then
-    sudo rm "$EXECUTABLE_NAME_APPIMAGE"
-  fi
+  echo "> sudo $PACKAGE_MANAGER $PACKAGE_MANAGER_INSTALL_COMMAND $ARTIFACT_PATH"
+  sudo $PACKAGE_MANAGER $PACKAGE_MANAGER_INSTALL_COMMAND "$ARTIFACT_PATH"
 fi
 
 if echo "$XDG_CURRENT_DESKTOP" | grep -iqF "gnome";
@@ -106,20 +130,20 @@ then
     # Adding the keyboard shortcut
     # Fetching all custom keyboard shortcuts
     CUSTOM_KEYBINDINGS_LIST=$(gsettings get org.gnome.settings-daemon.plugins.media-keys custom-keybindings)
-    if ! echo "$CUSTOM_KEYBINDINGS_LIST" | grep -q "x11-emoji-picker"
+    if ! echo "$CUSTOM_KEYBINDINGS_LIST" | grep -q "$PROJECT_REPO"
     then
       if [ "$CUSTOM_KEYBINDINGS_LIST" = "@as []" ] # creating new list if not exists
       then
-        CUSTOM_KEYBINDINGS_LIST="['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/x11-emoji-picker/']"
+        CUSTOM_KEYBINDINGS_LIST="['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/$PROJECT_REPO/']"
       else # if already existing, adding our new element
         CUSTOM_KEYBINDINGS_LIST=$(echo "$CUSTOM_KEYBINDINGS_LIST" | sed 's/\[//' | sed 's/\]//')
-        CUSTOM_KEYBINDINGS_LIST="[$CUSTOM_KEYBINDINGS_LIST, '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/x11-emoji-picker/']"
+        CUSTOM_KEYBINDINGS_LIST="[$CUSTOM_KEYBINDINGS_LIST, '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/$PROJECT_REPO/']"
       fi
 
       gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "$CUSTOM_KEYBINDINGS_LIST" # update the list
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/x11-emoji-picker/ name "Emoji Picker" # set name
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/x11-emoji-picker/ command "$EXECUTABLE_NAME" # set command
-      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/x11-emoji-picker/ binding "<Super>period" # set shortcut
+      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/$PROJECT_REPO/ name "$PROJECT_NAME" # set name
+      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/$PROJECT_REPO/ command "$EXECUTABLE_NAME" # set command
+      gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/$PROJECT_REPO/ binding "<Super>period" # set shortcut
       echo "Keyboard shortcut added!"
     else
       echo "Keyboard shortcut already added."
@@ -142,7 +166,7 @@ then
     I3_CONFIG=~/.config/i3/config
 
     # Adding the keyboard shortcut
-    if ! cat $I3_CONFIG | grep -q "emoji-picker";
+    if ! cat $I3_CONFIG | grep -q "$EXECUTABLE_NAME";
     then
       echo "
 bindsym \$mod+period exec --no-startup-id $EXECUTABLE_NAME
@@ -154,4 +178,4 @@ bindsym \$mod+period exec --no-startup-id $EXECUTABLE_NAME
 fi
 
 echo ""
-echo "Emoji picker installed! 🎉"
+echo "$PROJECT_NAME installed! 🎉"
